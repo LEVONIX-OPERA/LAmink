53dbdf7879b552942061a0368475a331a2e2b681
TROJAN-H0RSE <102463714+TROJAN-H0RSE@users.noreply.github.com>
2022-03-31 19:40:27 +1100

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,1 +1,20 @@
 @echo off
+cls
+goto main
+
+:main
+cls
+color 0f
+echo Booting from [CD-DEV]...
+ping localhost -n 2 >nul
+goto load
+
+:load
+cls
+color 9f
+echo WELCOME 
+ping localhost -n 2 >nul
+echo WELCOME TO
+ping localhost -n 2 >nul
+cls
+echo WELCOME TO 
